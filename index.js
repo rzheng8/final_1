0a04f31fe9e972ace5b4d76d0da052fa7f2bd534
rzheng8 <168156095+rzheng8@users.noreply.github.com>
2024-05-17 22:03:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-require('dotenv').config()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
-const sensorData1 = require('./routes/sensorData1');
-const sensorData2 = require('./routes/sensorData2');
+const sensorData1 = require("./routes/sensorData1");
+const sensorData2 = require("./routes/sensorData2");
 
 app.use("/api/v1/table1", sensorData1);
 app.use("/api/v1/table2", sensorData2);
 
-app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
